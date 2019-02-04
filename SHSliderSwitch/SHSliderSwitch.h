e26a8cc4d05d5bfa69497d4318f5d04bad2c1bb6
Focaloid Technologies <shezad@focaloid.com>
2019-02-04 17:44:35 +0530

    Initial Commit

---
+++ SHSliderSwitch/SHSliderSwitch.h
@@ -0,0 +1,19 @@
+//
+//  SHSliderSwitch.h
+//  SHSliderSwitch
+//
+//  Created by Focaloid Technologies on 04/02/19.
+//  Copyright © 2019 iamShezzad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHSliderSwitch.
+FOUNDATION_EXPORT double SHSliderSwitchVersionNumber;
+
+//! Project version string for SHSliderSwitch.
+FOUNDATION_EXPORT const unsigned char SHSliderSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHSliderSwitch/PublicHeader.h>
+
+
